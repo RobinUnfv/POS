875c037d327fa059947e86addfd7ee23012e9982
Robin-Casa <sant4n4.1@gmail.com>
2025-09-06 07:34:48 -0500

    Modificando la venta de venta

--- src/main/java/com/robin/pos/controller/VentaController.java
+++ src/main/java/com/robin/pos/controller/VentaController.java
@@ -1,4 +1,84 @@
 package com.robin.pos.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class VentaController {
+
+    @FXML
+    private Button agregarProductoButton;
+
+    @FXML
+    private Button btnBoleta;
+
+    @FXML
+    private ImageView btnBuscarCliente;
+
+    @FXML
+    private ImageView btnBuscarReniec;
+
+    @FXML
+    private Button btnFactura;
+
+    @FXML
+    private Button buscarClienteButton;
+
+    @FXML
+    private ComboBox<?> cbxDocIdentidad;
+
+    @FXML
+    private ComboBox<?> cbxFormaPago;
+
+    @FXML
+    private ComboBox<?> cbxMoneda;
+
+    @FXML
+    private ComboBox<?> cbxMotConting;
+
+    @FXML
+    private TextField descripcionProductoField;
+
+    @FXML
+    private TextField lblIgv;
+
+    @FXML
+    private TextField lblSubTotal;
+
+    @FXML
+    private TextField lblTotGravada;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private Button nuevoClienteButton;
+
+    @FXML
+    private TableView<?> tProducto;
+
+    @FXML
+    private TextArea txtDireccion;
+
+    @FXML
+    private DatePicker txtFechaVenta;
+
+    @FXML
+    private TextField txtNumDoc;
+
+    @FXML
+    private TextField txtPago;
+
+    @FXML
+    private TextField txtRazSocNom;
+
+    @FXML
+    private TextField txtVuelto;
+
 }
