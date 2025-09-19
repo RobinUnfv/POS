deeb59c87d3acc1a8acffb7a0df8279310e55838
Robin-Casa <sant4n4.1@gmail.com>
2025-09-18 23:54:14 -0500

    Agregando los atributos a la clase Ciente

--- src/main/java/com/robin/pos/model/Cliente.java
+++ src/main/java/com/robin/pos/model/Cliente.java
@@ -7,12 +7,30 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Cliente {
-    private final IntegerProperty idCliente = new SimpleIntegerProperty();
-    private final StringProperty tipoDocumento = new SimpleStringProperty();
-    private final StringProperty numDocumento = new SimpleStringProperty();
+    private final StringProperty noCliente = new SimpleStringProperty();
     private final StringProperty nombre = new SimpleStringProperty();
+    private final StringProperty telefono = new SimpleStringProperty();
+    private final StringProperty ruc = new SimpleStringProperty();
+    private final StringProperty tipoCliente = new SimpleStringProperty();
+    private final StringProperty tipoPersona = new SimpleStringProperty();
+    private final StringProperty nuDocumento = new SimpleStringProperty();
+    private final StringProperty tipoDocumento = new SimpleStringProperty();
+    private final StringProperty email = new SimpleStringProperty();
     private final StringProperty direccion = new SimpleStringProperty();
+    private final StringProperty codiDepa = new SimpleStringProperty();
+    private final StringProperty codiProv = new SimpleStringProperty();
+    private final StringProperty codiDist = new SimpleStringProperty();
+    private final StringProperty estabSunat = new SimpleStringProperty();
 
-    
+    public String getNoCliente() {
+        return noCliente.get();
+    }
 
+    public StringProperty noClienteProperty() {
+        return noCliente;
+    }
+
+    public void setNoCliente(String noCliente) {
+        this.noCliente.set(noCliente);
+    }
 }
