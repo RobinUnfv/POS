2006a19d600af11f6704d0eaa90b59e267f7ac10
Robin-Casa <sant4n4.1@gmail.com>
2025-11-20 22:35:30 -0500

    Ocultando campos de cliente

--- src/main/java/com/robin/pos/controller/ClienteController.java
+++ src/main/java/com/robin/pos/controller/ClienteController.java
@@ -106,11 +106,24 @@ public class ClienteController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        configurarTipoDocumento();
+        configurarRadioButon();
+
+        this.cbxTipDoc.valueProperty().addListener((obs, oldVal, newVal) -> {
+            updateVisibilityByTipoDoc(newVal);
+            // si además necesita reconfigurar el formato del número:
+            Metodos.configuracionNumeroDocumento(this.txtNumDoc, newVal != null ? newVal : "RUC");
+        });
+    }
+
+    private void configurarTipoDocumento() {
         this.cbxTipDoc.getItems().addAll("RUC", "DNI","CE");
         this.cbxTipDoc.setValue("RUC");
         Metodos.configuracionNumeroDocumento(this.txtNumDoc ,"RUC");
         this.txtNumDoc.requestFocus();
+    }
 
+    private void configurarRadioButon() {
         ToggleGroup tipoDocGroup = new ToggleGroup();
         ToggleGroup tipoPersonaGroup = new ToggleGroup();
 
@@ -122,6 +135,43 @@ public class ClienteController implements Initializable {
         // Seleccionar uno por defecto
         rbnJuridico.setSelected(true);
         rbnNacional.setSelected(true);
-
     }
+
+    private void updateVisibilityByTipoDoc(String tipoDoc) {
+        boolean isRUC = "RUC".equals(tipoDoc);
+        boolean isDNI = "DNI".equals(tipoDoc);
+        boolean isCE = "CE".equals(tipoDoc);
+
+        // Mostrar/ocultar campos según el tipo de documento
+        lblNacionalidad.setVisible(isDNI || isCE);
+        rbnNacional.setVisible(isDNI || isCE);
+        rbnExtranjero.setVisible(isDNI || isCE);
+
+        // Si es RUC ocultar campos de persona natural (gpTres) y mostrar Razon Social (gpCuatro)
+        gpTres.setVisible(!isRUC);
+        gpTres.setManaged(!isRUC);
+
+        gpCuatro.setVisible(isRUC);
+        gpCuatro.setManaged(isRUC);
+
+        // Si se quiere un comportamiento especial para CE u otros, se puede añadir aquí.
+        // Por ejemplo, si CE debe comportarse como DNI:
+        if (tipoDoc.equalsIgnoreCase("CE")) {
+            gpTres.setVisible(true);
+            gpTres.setManaged(true);
+            gpCuatro.setVisible(false);
+            gpCuatro.setManaged(false);
+        }
+
+        // Ajustar etiquetas
+        if (isRUC) {
+            lblTipNum.setText("Número RUC:");
+        } else if (isDNI) {
+            lblTipNum.setText("Número DNI:");
+        } else if (isCE) {
+            lblTipNum.setText("Número CE:");
+        }
+    }
+
+
 }
