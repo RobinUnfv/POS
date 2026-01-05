1d80ef99943819cc77fbc90f45ffa94cf37822b0
Robin-Casa <sant4n4.1@gmail.com>
2026-01-05 07:46:03 -0500

    Cambiando el icono de inicio

--- src/main/java/com/robin/pos/MainApp.java
+++ src/main/java/com/robin/pos/MainApp.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -16,10 +17,14 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/com/robin/pos/fxml/Venta.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/com/robin/pos/fxml/Dashboard.fxml"));
+        // ========== AGREGAR ÍCONO ==========
+        // Opción 1: Desde recursos del proyecto
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/robin/pos/imagenes/logos-nexer.png")));
+
         Scene scene = new Scene(fxmlLoader.load(), 1270, 700);
         stage.setMaximized(true);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-
+        stage.setTitle("VENTA");
         stage.setScene(scene);
         stage.show();
     }
